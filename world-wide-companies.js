b17c57c26b07299c8b9d2518db55c750c9b23fe5
Renato <renato@hiparc.com>
2020-10-23 17:31:05 -0300

    created js files that contains the data that will be in the index.html

---
+++ world-wide-companies.js
@@ -0,0 +1,242 @@
+var Wcompanies = [
+    {
+        "company": "10up",
+        "website": "http://10up.com/careers/"
+    },
+    {
+        "company": "15five",
+        "website": "http://www.15five.com/"
+    },
+    {
+        "company": "ADireto",
+        "website": "https://adireto.com/"
+    },
+    {
+        "company": "AE Studio",
+        "website": "https://ae.studio/join-us"
+    },
+    {
+        "company": "Anchor Loans",
+        "website": "http://www.anchorloans.com/"
+    },
+    {
+        "company": "Aubay",
+        "website": "https://www.aubay.pt/"
+    },
+    {
+        "company": "Auth0",
+        "website": "https://auth0.com/jobs"
+    },
+    {
+        "company": "Automattic",
+        "website": "https://automattic.com/work-with-us/"
+    },
+    {
+        "company": "BairesDev",
+        "website": "http://www.bairesdev.com/"
+    },
+    {
+        "company": "Bandcamp",
+        "website": "https://bandcamp.com/jobs"
+    },
+    {
+        "company": "Bitcointrade",
+        "website": "https://www.bitcointrade.com.br"
+    },
+    {
+        "company": "Codestunts",
+        "website": "https://codestunts.com"
+    },
+    {
+        "company": "Clevertech",
+        "website": "http://www.clevertech.biz/"
+    },
+    {
+        "company": "Close.io",
+        "website": "https://jobs.lever.co/close.io/"
+    },
+    {
+        "company": "Collabora",
+        "website": "https://www.collabora.com/careers.html"
+    },
+    {
+        "company": "Crave Food",
+        "website": "http://sourcewhatsgood.com/"
+    },
+    {
+        "company": "Crossover",
+        "website": "http://www.crossover.com"
+    },
+    {
+        "company": "DevSquad",
+        "website": "https://devsquad.com"
+    },
+    {
+        "company": "Discogs",
+        "website": "http://www.discogs.com/jobs"
+    },
+    {
+        "company": "Doist",
+        "website": "https://doist.com/jobs/"
+    },
+    {
+        "company": "Elastic",
+        "website": "https://www.elastic.co/about/careers"
+    },
+    {
+        "company": "Gruntwork",
+        "website": "https://gruntwork.io/careers/"
+    },
+    {
+        "company": "Heroku",
+        "website": "https://www.heroku.com/"
+    },
+    {
+        "company": "Hivelocity",
+        "website": "https://www.hivelocity.net/about/careers/"
+    },
+    {
+        "company": "IBM",
+        "website": "http://www.ibm.com/br-pt/"
+    },
+    {
+        "company": "Lev Interactive",
+        "website": "http://lev-interactive.com/"
+    },
+    {
+        "company": "Living Social",
+        "website": "https://www.livingsocial.com/"
+    },
+    {
+        "company": "Libring",
+        "website": "https://libring.com/careers-developers/"
+    },
+    {
+        "company": "Loadsmart",
+        "website": "https://loadsmart.com/careers/"
+    },
+    {
+        "company": "LocalLabs",
+        "website": "http://www.locallabs.com/"
+    },
+    {
+        "company": "MadeWithLove",
+        "website": "https://madewithlove.be/"
+    },
+    {
+        "company": "Mcontigo",
+        "website": "https://mcontigo.com/"
+    },
+    {
+        "company": "Modern Tribe",
+        "website": "http://tri.be/about/join-our-team/"
+    },
+    {
+        "company": "Mindera",
+        "website": "https://mindera.com/"
+    },
+    {
+        "company": "MustHaveMenus",
+        "website": "http://www.musthavemenus.com/"
+    },
+    {
+        "company": "OpenLaw",
+        "website": "https://openlaw.breezy.hr"
+    },
+    {
+        "company": "OSF",
+        "website": "https://osf.digital/careers/jobs"
+    },
+    {
+        "company": "Platform.sh",
+        "website": "https://platform.sh/website/careers"
+    },
+    {
+        "company": "Quez Media",
+        "website": "http://www.quezmedia.com/"
+    },
+    {
+        "company": "RedHat",
+        "website": "https://careers-redhat.icims.com/jobs/search?ss=1&searchLocation=12920--Remote"
+    },
+    {
+        "company": "Remote Crew",
+        "website": "https://remotecrew.io"
+    },
+    {
+        "company": "RocketPower",
+        "website": "https://www.rocketpower.io/"
+    },
+    {
+        "company": "Scalable Path",
+        "website": "https://www.scalablepath.com/apply"
+    },
+    {
+        "company": "ScrapingHub",
+        "website": "http://scrapinghub.com/jobs/"
+    },
+    {
+        "company": "Shaw And Partners",
+        "website": "https://app.shawandpartners.com/"
+    },
+    {
+        "company": "Soshace",
+        "website": "https://soshace.com/careers"
+    },
+    {
+        "company": "Stack Exchange",
+        "website": "http://stackexchange.com/work-here"
+    },
+    {
+        "company": "Stickermule",
+        "website": "https://www.stickermule.com/"
+    },
+    {
+        "company": "Teamed",
+        "website": "http://www.teamed.io/"
+    },
+    {
+        "company": "Telnyx",
+        "website": "https://telnyx.com/website/careers"
+    },
+    {
+        "company": "Theorem",
+        "website": "https://theorem.co/careers"
+    },
+    {
+        "company": "TIDY",
+        "website": "https://tidy.com/"
+    },
+    {
+        "company": "Toggl",
+        "website": "https://toggl.com/jobs/"
+    },
+    {
+        "company": "Toptal",
+        "website": "http://www.toptal.com/"
+    },
+    {
+        "company": "Truelogic",
+        "website": "https://www.truelogicsoftware.com/"
+    },
+    {
+        "company": "X-Team",
+        "website": "http://www.x-team.com/"
+    },
+    {
+        "company": "Yoko Co.",
+        "website": "https://yokoco.com/careers/"
+    },
+    {
+        "company": "Zapier",
+        "website": "https://zapier.com/jobs/"
+    },
+    {
+        "company": "ZenDesk",
+        "website": "https://www.zendesk.com/jobs/remote/"
+    },
+    {
+        "company": "Zup",
+        "website": "https://www.zup.com.br/"
+    }
+]
